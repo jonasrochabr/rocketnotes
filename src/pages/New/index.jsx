3831f20c16bf31d2fe4bdb7364061d5076c79bfe
Jonas Rocha <jonasrochabr@gmail.com>
2023-07-13 09:49:38 -0400

    text area complete

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
+import { Textarea } from "../../components/Textarea"
 
 import { Container, Form } from "./styles"
 
@@ -17,6 +18,7 @@ export function New(){
                     </header>
 
                     <Input placeholder="Título"/>
+                    <Textarea placeholder="Observações"/>
                 </Form>
             </main>
         </Container>
